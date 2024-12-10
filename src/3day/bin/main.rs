d26974c9d765d12318963fd881fd39f8cd6f4c6c
teehee <dynamTypedDisaste@proton.me>
2024-12-10 13:18:48 +1100

    day 3 part 1
    functional rewrite of imperative solution for day 3 part 1

--- src/3day/bin/main.rs
+++ src/3day/bin/main.rs
@@ -1,56 +1,37 @@
-use std::fs::File;
 use std::io::Read;
-use std::error::Error;
+use std::fs::File;
 
-fn main()-> Result<(), Box<dyn Error>> { 
-    let sample_data: String = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))".to_string();
+fn print_type_of<T>(_: &T) {
+    println!("{}", std::any::type_name::<T>());
+}
+
+fn part_one(input_data: String) { 
+    let final_step: i32 = 
+        input_data
+        .split("mul(")
+        .map(|option| {
+            option
+                .chars()
+                .take_while(|&c| c != ')')
+                .collect::<String>()
+                .split_once(',')
+                .and_then(|(a,b)| {
+                    a.trim().parse::<i32>().ok().zip(b.trim().parse::<i32>().ok())
+                })
+                .map(|(a,b)| a*b)
+                .unwrap_or(0)
+        }).sum();
+        //.filter(|&x| x != 0);
+        //.sum()
+        print_type_of(&final_step);
+        println!("{final_step}");
+}
+
+fn main() { 
     let mut contents = String::new();
     let mut f = File::open("inps/3.txt").expect("open");
     f.read_to_string(&mut contents).expect("open again");
-    //contents.chars()
-    //    .for_each(|c| println!("{}", c));
-    
-    let mut expression_list: Vec<i32> = Vec::new();
-    //dbg!("{contents}");
-    let mut iter_chars = sample_data.chars().peekable();
-    while let Some(c) = iter_chars.next() {
-        match c {
-            'm' => if iter_chars.clone().take(2).collect::<String>() == "ul" {
-                if let Some('(') = iter_chars.next() { 
-                    let mut first_val = String::new();
-
-                    while let Some(c) = iter_chars.next() {
-                        if c.is_digit(10) { // radix of 10
-                            first_val.push(c);
-                    } else if c == ',' {
-                        break;
-                    } else {
-                        break;
-                    }
-                }
-                let mut second_val = String::new();
-                while let Some(c) = iter_chars.next() { 
-                    if c.is_digit(10) {
-                        second_val.push(c);
-                    } else if c == ')' {
-                        break;
-                    } else {
-                        break;
-                    }
-                }
-                if !first_val.is_empty() && !second_val.is_empty() {
-                    expression_list.push(first_val.parse::<i32>().unwrap() * second_val.parse::<i32>().unwrap());
-                }
-                }
-
-            }
-            _ => println!("oh oh"),
-
-        }
-    }
-    let temp_ans: i32  = expression_list.iter().fold(0, |acc, val| acc + val);
-    println!("{temp_ans}");
-    Ok(())
+    part_one(contents);
 }
 
 #[cfg(test)]
